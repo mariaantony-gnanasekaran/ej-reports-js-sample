518fe67544303c5a0bc8e34651bbc9f7be788c67
mariaantony.gnanasekaran <mariaantony.gnanasekaran@syncfusion.com>
2019-06-07 16:46:06 +0530

    Updated gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require("gulp");
 const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config.js');
 const open = require('open');
 const port = 9000;
+const { rm, mkdir, cp } = require('shelljs');
 
-gulp.task('serve', function () {
+gulp.task('serve', () => {
   const server = new WebpackDevServer(webpack(config));
-  server.listen(port, 'localhost', function (err) {
+  server.listen(port, 'localhost', (err) => {
     if (err) {
       console.log(err);
     }
@@ -15,4 +17,14 @@ gulp.task('serve', function () {
     open('http://localhost:' + port + '/');
   });
 
+});
+
+
+gulp.task('build', () => {
+  rm('-rf', 'dist');
+  mkdir('-p', 'dist');
+  cp('-r', ['index.html', 'favicon.ico', 'assets', 'src'], 'dist');
+  return gulp.src('.')
+    .pipe(webpackStream(require('./webpack.config.js'), webpack))
+    .pipe(gulp.dest('dist'));
 });
